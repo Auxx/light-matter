3fe28ab382a1721b2723fc75f756a1cfc413c43d
Aleksandr 'Aux' Ivanov <aux@hexmode.org>
2026-04-24 16:48:03 +0100

    Code restructure

--- apps/web/src/app/app.routes.ts
+++ apps/web/src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Route } from '@angular/router';
-import { startupGuard } from './system/guards/startup/startup.guard';
-import { Root } from './system/pages/root/root';
+import { startupGuard } from '../system/guards/startup/startup.guard';
+import { Root } from '../system/pages/root/root';
 
 export const appRoutes: Route[] = [
   { path: '', component: Root, canActivate: [ startupGuard ] },
-  { path: 'view', loadChildren: () => import('./viewer/viewer.routes').then(m => m.routes) },
-  { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.routes').then(m => m.routes) }
+  { path: 'view', loadChildren: () => import('../viewer/viewer.routes').then(m => m.routes) },
+  { path: 'welcome', loadChildren: () => import('../welcome/welcome.routes').then(m => m.routes) }
 ];
