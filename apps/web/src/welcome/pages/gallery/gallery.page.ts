eb6faedcd321a7623d066d3e4f23154eff68923b
Aleksandr 'Aux' Ivanov <aux@hexmode.org>
2026-04-25 16:18:36 +0100

    Added basic gallery location display

--- apps/web/src/welcome/pages/gallery/gallery.page.ts
+++ apps/web/src/welcome/pages/gallery/gallery.page.ts
@@ -1,11 +1,21 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { AsyncPipe, JsonPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { GalleryLocations } from '../../../gallery/services/gallery-locations/gallery-locations';
+import { DefaultPipe } from '../../../system/pipes/default/default.pipe';
 
 @Component({
   selector: 'app-gallery',
-  imports: [],
+  imports: [
+    AsyncPipe,
+    DefaultPipe,
+    JsonPipe
+  ],
   templateUrl: './gallery.page.html',
   styleUrl: './gallery.page.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class GalleryPage {
+  private readonly galleryLocations = inject(GalleryLocations);
+
+  readonly locations$ = this.galleryLocations.locations();
 }
