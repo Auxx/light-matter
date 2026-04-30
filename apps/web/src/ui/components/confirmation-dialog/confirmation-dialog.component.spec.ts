0411f2995692624dc641f486a344640aed11c05d
Aleksandr 'Aux' Ivanov <aux@hexmode.org>
2026-04-30 17:17:19 +0100

    Fixed unit tests

--- apps/web/src/ui/components/confirmation-dialog/confirmation-dialog.component.spec.ts
+++ apps/web/src/ui/components/confirmation-dialog/confirmation-dialog.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 import { ConfirmationDialogComponent } from './confirmation-dialog.component';
 
@@ -8,7 +9,10 @@ describe('ConfirmationDialogComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ ConfirmationDialogComponent ]
+      imports: [ ConfirmationDialogComponent ],
+      providers: [
+        { provide: MAT_DIALOG_DATA, useValue: { title: 'Are you sure?', description: 'This is a test' } }
+      ]
     })
       .compileComponents();
 
