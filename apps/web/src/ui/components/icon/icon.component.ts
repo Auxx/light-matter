59d5e99dec76660b8df834823fe1c874c0b0cbf9
Aleksandr 'Aux' Ivanov <aux@hexmode.org>
2026-05-08 15:48:09 +0100

    Colour pallete WIP

--- apps/web/src/ui/components/icon/icon.component.ts
+++ apps/web/src/ui/components/icon/icon.component.ts
@@ -2,6 +2,10 @@ import { ChangeDetectionStrategy, Component, computed, inject, input } from '@an
 import { DomSanitizer } from '@angular/platform-browser';
 import { iconMapping, IconName } from './icon.types';
 
+export const allIconSizes = [ 'medium', 'small', 'x-small', 'large', 'x-large' ] as const;
+
+export type IconSize = typeof allIconSizes[number];
+
 @Component({
   selector: 'app-icon',
   imports: [],
@@ -9,11 +13,14 @@ import { iconMapping, IconName } from './icon.types';
   styleUrl: './icon.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
   host: {
-    '[innerHTML]': 'iconCode()'
+    '[innerHTML]': 'iconCode()',
+    '[style.--icon-size]': '`var(--icon-size-${size()})`'
   }
 })
 export class IconComponent {
   readonly icon = input.required<IconName>();
+
+  readonly size = input<IconSize>(allIconSizes[0]);
 
   protected readonly iconCode = computed(() => this.sanitizer.bypassSecurityTrustHtml(iconMapping[this.icon()]));
 
