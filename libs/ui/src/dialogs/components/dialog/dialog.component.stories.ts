808660648d4ef10cd7264c77f9048f520c37bb96
Aleksandr 'Aux' Ivanov <aux@hexmode.org>
2026-05-14 19:10:05 +0100

    Added DialogComponent stories

--- libs/ui/src/dialogs/components/dialog/dialog.component.stories.ts
+++ libs/ui/src/dialogs/components/dialog/dialog.component.stories.ts
@@ -45,3 +45,45 @@ export const Primary: Story = {
     };
   }
 };
+
+/**
+ * It is possible to customise most visual aspects of `ActionButton` using CSS variables.
+ * These variables can be set either via a stylesheet or using Angular `[style]` binding,
+ * as shown in the example below. The `[style]` binding can also accept a single object
+ * containing multiple variables.
+ *
+ * The following CSS variables can be used for custom styling:
+ *
+ * | Variable                   |
+ * |----------------------------|
+ * | --dialog-background-color  |
+ * | --header-background-color  |
+ * | --content-background-color |
+ * | --footer-background-color  |
+ * | --gap                      |
+ * | --padding-vertical         |
+ * | --padding-horizontal       |
+ * | --header-height            |
+ * | --footer-height            |
+ */
+export const CustomStyling: Story = {
+  render: props => {
+    return {
+      props,
+      template: `
+      <ui-dialog [style.--header-background-color]="'#122'">
+        <header>
+          <ui-title>Header</ui-title>
+          <div>Info</div>
+        </header>
+        <section>
+          <div>Body</div>
+        </section>
+        <footer>
+          <ui-action-button variant="accent">OK</ui-action-button>
+          <ui-action-button>Cancel</ui-action-button>
+        </footer>
+      </ui-dialog>`
+    };
+  }
+};
