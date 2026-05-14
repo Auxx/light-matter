3d9f77cf7d45fb827c91b12f19cd9412a20bf361
Aleksandr 'Aux' Ivanov <aux@hexmode.org>
2026-05-14 17:24:04 +0100

    Updated ToolbarComponent

--- libs/ui/src/layouts/components/toolbar/toolbar.component.stories.ts
+++ libs/ui/src/layouts/components/toolbar/toolbar.component.stories.ts
@@ -40,3 +40,32 @@ export const Primary: Story = {
     };
   }
 };
+
+/**
+ * It is possible to customise most visual aspects of `Toolbar` using CSS variables.
+ * These variables can be set either via a stylesheet or using Angular `[style]` binding,
+ * as shown in the example below. The `[style]` binding can also accept a single object
+ * containing multiple variables.
+ *
+ * The following CSS variables can be used for custom styling:
+ *
+ * | Variable |
+ * |----------|
+ * | --gap    |
+ * | --height |
+ */
+export const CustomStyling: Story = {
+  render: props => {
+    return {
+      props,
+      template: `
+      <ui-toolbar [style.--height]="'60px'">
+        <ui-action-button><ui-icon icon="chevronLeft" size="large"/></ui-action-button>
+        <ui-title>Properties</ui-title>
+        <aside>
+          <ui-action-button><ui-icon icon="moreVertical" size="large"/></ui-action-button>
+        </aside>
+      </ui-toolbar>`
+    };
+  }
+};
