4fbd24024a76661936de2301ac505d0505f8e2ce
Aleksandr 'Aux' Ivanov <aux@hexmode.org>
2026-06-16 11:53:15 +0100

    Code clean up

--- apps/web/src/system/guards/startup/startup.guard.ts
+++ apps/web/src/system/guards/startup/startup.guard.ts
@@ -4,6 +4,7 @@ import 'internal-api';
 import { firstValueFrom, map } from 'rxjs';
 import { GalleryState } from '../../../gallery/services/gallery-state/gallery-state';
 import { FileSystem } from '../../../ipc/file-system';
+import { ProcessManager } from '../../../ipc/process-manager';
 import { ViewNavigator } from '../../../viewer/services/view-navigator/view-navigator';
 
 export const startupGuard: CanActivateFn = async () => {
@@ -11,9 +12,10 @@ export const startupGuard: CanActivateFn = async () => {
   const viewNavigator = inject(ViewNavigator);
   const fileSystem = inject(FileSystem);
   const galleryState = inject(GalleryState);
+  const processManager = inject(ProcessManager);
 
-  const argv = await window.desktop.ProcessManager.argv();
-  const isPackaged = await window.desktop.ProcessManager.isPackaged();
+  const argv = await processManager.argv();
+  const isPackaged = await processManager.isPackaged();
 
   if (isPackaged && argv._.length === 1 && typeof argv._[0] === 'string') {
     const image = argv._[0];
