bf55735abe4d62ab2049c5fa985f10b9ba9ab6f8
Aleksandr 'Aux' Ivanov <aux@hexmode.org>
2026-06-19 16:50:53 +0100

    Added sorting menu boilerplate

--- apps/web/src/gallery/components/image-grid/image-grid.component.ts
+++ apps/web/src/gallery/components/image-grid/image-grid.component.ts
@@ -1,6 +1,22 @@
+import { CdkMenu, CdkMenuItem, CdkMenuTrigger } from '@angular/cdk/menu';
 import { ChangeDetectionStrategy, Component, effect, ElementRef, input, output, viewChild } from '@angular/core';
-import { TitleComponent, ToolbarComponent } from '@light-matter/ui';
+import {
+  ActionButtonComponent,
+  IconComponent,
+  MenuSeparatorComponent,
+  PopupMenuComponent,
+  StopPropagation,
+  TextComponent,
+  TitleComponent,
+  ToolbarComponent
+} from '@light-matter/ui';
 import { FileInfo } from 'internal-api';
+import {
+  allSortDirections,
+  allSortTypes,
+  SortDirection,
+  SortType
+} from '../../services/gallery-state/gallery-state.types';
 import { ImagesComponent } from '../images/images.component';
 
 @Component({
@@ -8,7 +24,16 @@ import { ImagesComponent } from '../images/images.component';
   imports: [
     ImagesComponent,
     ToolbarComponent,
-    TitleComponent
+    TitleComponent,
+    ActionButtonComponent,
+    IconComponent,
+    StopPropagation,
+    CdkMenuTrigger,
+    CdkMenu,
+    CdkMenuItem,
+    PopupMenuComponent,
+    TextComponent,
+    MenuSeparatorComponent
   ],
   templateUrl: './image-grid.component.html',
   styleUrl: './image-grid.component.scss',
@@ -20,9 +45,14 @@ export class ImageGridComponent {
 
   readonly selectedLocation = input.required<string | null>();
 
+  readonly sortBy = input<SortType>(allSortTypes[0]);
+
+  readonly sortDirection = input<SortDirection>(allSortDirections[0]);
+
   /* Outputs */
   readonly selected = output<FileInfo>();
 
+  /* State */
   private readonly scrollBox = viewChild<ElementRef<HTMLDivElement>>('scrollBox');
 
   /* Constructor */
