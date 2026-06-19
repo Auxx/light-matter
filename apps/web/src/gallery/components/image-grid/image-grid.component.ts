c94d34f67a2e4f51362316d5b796cb7f52dba1c2
Aleksandr 'Aux' Ivanov <aux@hexmode.org>
2026-06-19 16:07:33 +0100

    Fixed a bug when scroll location would stick when switching between gallery folders (#50)

--- apps/web/src/gallery/components/image-grid/image-grid.component.ts
+++ apps/web/src/gallery/components/image-grid/image-grid.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, input, output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, effect, ElementRef, input, output, viewChild } from '@angular/core';
 import { TitleComponent, ToolbarComponent } from '@light-matter/ui';
 import { FileInfo } from 'internal-api';
 import { ImagesComponent } from '../images/images.component';
@@ -22,4 +22,20 @@ export class ImageGridComponent {
 
   /* Outputs */
   readonly selected = output<FileInfo>();
+
+  private readonly scrollBox = viewChild<ElementRef<HTMLDivElement>>('scrollBox');
+
+  /* Constructor */
+  constructor() {
+    effect(() => {
+      this.selectedLocation();
+      const scrollBox = this.scrollBox();
+
+      if (scrollBox === undefined) {
+        return;
+      }
+
+      scrollBox.nativeElement.scrollTo(0, 0);
+    });
+  }
 }
